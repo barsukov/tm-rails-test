94c8e63fd9a90b5963d65226cc07bb4ced1cdc1f
vladimirb <pro.barsukov@gmail.com>
2013-12-14 17:33:45 +0400

    compleate audit task , compleate cucumber test for cover all needed functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131212181402) do
+ActiveRecord::Schema.define(:version => 20131213215644) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -47,6 +47,11 @@ ActiveRecord::Schema.define(:version => 20131212181402) do
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
 
+  create_table "campaign_platforms", :force => true do |t|
+    t.integer "campaign_id"
+    t.integer "platform_id"
+  end
+
   create_table "campaigns", :force => true do |t|
     t.string   "name"
     t.integer  "budget"
@@ -56,9 +61,19 @@ ActiveRecord::Schema.define(:version => 20131212181402) do
 
   create_table "platforms", :force => true do |t|
     t.string   "name"
-    t.integer  "campaign_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
+
+  create_table "versions", :force => true do |t|
+    t.string   "item_type",  :null => false
+    t.integer  "item_id",    :null => false
+    t.string   "event",      :null => false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+  end
+
+  add_index "versions", ["item_type", "item_id"], :name => "index_versions_on_item_type_and_item_id"
 
 end
